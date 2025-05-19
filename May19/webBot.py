1a118bbdfa3c16e3a4619807c791b3ae0156c6c7
abhhiramanil369 <abhiramforcoding369@gmail.com>
2025-05-19 13:42:08 +0530

    latest updates added

--- May19/webBot.py
+++ May19/webBot.py
@@ -1,3 +1,6 @@
 import asyncio
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import WebDriverException
+from gemini_utils import configure_gemini, summarize_with_gemini
