17015e386e0526c1e44c506f0486ee2b23e991c5
abhhiramanil369 <abhiramforcoding369@gmail.com>
2025-05-20 17:06:36 +0530

    api key change

--- May20/tools/gemini_flash.py
+++ May20/tools/gemini_flash.py
@@ -5,9 +5,9 @@ class GeminiFlashTool:
     name = "llm"
 
     def __init__(self, api_key=None, model="gemini-1.5-flash"):
-        self.api_key = api_key or os.environ.get("GOOGLE_API_KEY")
+        
         self.model = model
-        genai.configure(api_key=self.api_key)
+        genai.configure(api_key="AIzaSyCi7hA6xxA7lUx-BaUXFpRxipONhGcQfBw")
         self.client = genai.GenerativeModel(model)
 
     async def complete(self, prompt):
